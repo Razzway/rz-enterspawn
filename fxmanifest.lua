19b88888f6bcb0b7af1a7ad97eac0543aa7a49c7
Razzway <72881565+Razzway@users.noreply.github.com>
2022-07-18 22:14:20 +0200

    fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 use_fxv2_oal 'yes'
 
-author 'Razzway''
+author 'Razzway'
 version '1.0.0'
 
 shared_scripts {
@@ -11,14 +11,14 @@ shared_scripts {
 }
 
 client_scripts {
-    'libs/RageUI//RMenu.lua',
-    'libs/RageUI//menu/RageUI.lua',
-    'libs/RageUI//menu/Menu.lua',
-    'libs/RageUI//menu/MenuController.lua',
-    'libs/RageUI//components/*.lua',
-    'libs/RageUI//menu/elements/*.lua',
-    'libs/RageUI//menu/items/*.lua',
-    'libs/RageUI//menu/panels/*.lua',
+    'libs/RageUI/RMenu.lua',
+    'libs/RageUI/menu/RageUI.lua',
+    'libs/RageUI/menu/Menu.lua',
+    'libs/RageUI/menu/MenuController.lua',
+    'libs/RageUI/components/*.lua',
+    'libs/RageUI/menu/elements/*.lua',
+    'libs/RageUI/menu/items/*.lua',
+    'libs/RageUI/menu/panels/*.lua',
     'libs/RageUI/menu/windows/*.lua',
 
     'client/*.lua',
